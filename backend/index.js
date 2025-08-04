a68a2411a5b185c5e6c4507830ed06e8c4b66da4
Icaro Pegoraro Sakurai <icaropegsakurai@gmail.com>
2025-08-04 17:27:03 -0300

    Adicionando files e folders no backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API está rodando 🚀');
+});
+
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
